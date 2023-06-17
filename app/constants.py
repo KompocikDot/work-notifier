07449d0387f2aeabb90ae9fc78e38ba578776a66
Kacper Soczko <kacpersoczko@bluerider.software>
2023-06-17 21:09:05 +0200

    Add more notifier logic, change flake8 line-len to 88

--- app/constants.py
+++ app/constants.py
@@ -2,3 +2,5 @@ JUST_JOIN_IT_API_URL = "https://justjoin.it/api/offers"
 NO_FLUFF_JOBS_BASE_URL = "https://nofluffjobs.com/api/search/posting"
 PRACUJ_PL_BASE_URL = "https://www.pracuj.pl/praca/"
 BULLDOG_JOB_API_URL = "https://bulldogjob.pl/graphql"
+
+LOGGING_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
