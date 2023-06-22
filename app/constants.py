8258bcb5e3e4b5f2cb3c3ea56ff6e3198574432b
Kacper Soczko <kacpersoczko@bluerider.software>
2023-06-22 19:57:36 +0200

    Add precommit to github actions

--- app/constants.py
+++
@@ -1,5 +0,0 @@
-NO_FLUFF_JOBS_BASE_URL = "https://nofluffjobs.com/api/search/posting"
-PRACUJ_PL_BASE_URL = "https://www.pracuj.pl/praca/"
-BULLDOG_JOB_API_URL = (
-    "https://bulldogjob.pl/_next/data/qFgVDsu3Coo29CPw4w5P4/pl/companies/jobs/s/"
-)
